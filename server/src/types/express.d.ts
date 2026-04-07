9ea01b35129fce4a0a9863a9e046a8bbb9961d42
Dwayne Braboy <ehabitatllc@gmail.com>
2026-04-07 11:22:22 -0400

    Track Express request actor types

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,27 @@
+export {};
+
+type RequestActor = {
+  type: "board" | "agent" | "none";
+  userId?: string;
+  agentId?: string;
+  companyId?: string;
+  companyIds?: string[];
+  isInstanceAdmin?: boolean;
+  keyId?: string;
+  runId?: string;
+  source?: "local_implicit" | "session" | "board_key" | "agent_key" | "agent_jwt" | "none";
+};
+
+declare global {
+  namespace Express {
+    interface Request {
+      actor: RequestActor;
+    }
+  }
+}
+
+declare module "express-serve-static-core" {
+  interface Request {
+    actor: RequestActor;
+  }
+}
